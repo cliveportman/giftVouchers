382072194acf4647922c8ae3bdf0b4f561ff027f
cliveportman <clive@cliveportman.co.uk>
2017-04-05 14:58:51 +0100

    Initial commit

---
+++ GiftVouchersPlugin.php
@@ -0,0 +1,36 @@
+<?php
+namespace Craft;
+
+class GiftVouchersPlugin extends BasePlugin
+{
+
+	public function getName()
+	{
+		return 'Gift Vouchers';
+	}
+
+	public function getDescription()
+	{
+		return '';
+	}
+
+	public function getVersion()
+	{
+		return '1.0.0';
+	}
+
+	public function getDeveloper()
+	{
+		return 'Clive Portman';
+	}
+
+	public function getDeveloperUrl()
+	{
+		return 'https://www.theportman.co/';
+	}
+
+	public function init()
+	{
+	}
+
+}
